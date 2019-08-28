19ccfeff6286d288939747804c95301430f644f4
{dp7jm} <{dpjm@chef.net}>
2019-08-28 19:28:20 +0200

    Add project files.

---
+++ PosteAppCS/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PosteAppCS
+{
+
+    class MesageApp
+    {
+        private List<string> friendsPost = new List<string>();
+
+        public void addFriend(string name)
+        {
+            this.friendsPost.Add(name);
+
+        }
+
+        public string showFriends()
+        {
+
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
